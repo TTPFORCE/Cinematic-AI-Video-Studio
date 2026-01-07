a45cf5fa77475c7420f595cc06a33601efd35f7f
TTPFORCE <abdulnasirafg371@gmail.com>
2026-01-07 09:26:25 +0400

    Create App.tsx

---
+++ ui/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TimelineEditor from "./TimelineEditor";
+
+export default function App() {
+  return (
+    <div style={{ height: "100vh", background: "#0e0e0e", color: "#fff" }}>
+      <TimelineEditor />
+    </div>
+  );
+}
